2741c7b7630c1aadb5ec6b5f5bd3cd40eaec71ff
recusthein <bintangwijaya18@gmail.com>
2022-09-09 21:00:11 +0700

    push

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col">
                 <div class="page-description">
-                    <h1>Dashboard</h1>
+                    <h1>Dashboard </h1>
                 </div>
             </div>
         </div>
