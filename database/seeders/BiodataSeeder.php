fd69eeed6b42c4832c69e3f34a576d9f22636b82
zakkysurya <zakkysurya@gmail.com>
2022-09-11 01:04:27 +0700

    Correct BiodataSeeder

--- database/seeders/BiodataSeeder.php
+++ database/seeders/BiodataSeeder.php
@@ -16,7 +16,7 @@ class BiodataSeeder extends Seeder
      */
     public function run()
     {   
-        DB::table('users')->insert([
+        DB::table('biodata')->insert([
            'full_name' => 'admin',
            'nick_name' => 'lagi',
            'position' => 'be developer',
@@ -29,6 +29,7 @@ class BiodataSeeder extends Seeder
            'linkid' => Null,
            'job_description' => 'building simple and beautiful things for complex interfaces is what I enjoy most about my work. I am also interested in crafting beautiful minimal products and exploring worlds.',//[char:180],
            'self_description' => 'it is a kong established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using normal distribution of letters, as opposed to using content here, content here', //[char:250]
+           'user_id' => 1,
            'created_at' => Carbon::now(),
            'updated_at' => Carbon::now()
         ]);
