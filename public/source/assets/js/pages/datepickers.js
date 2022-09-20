c7e2e3f9ca9373b1b2b85e8edc53f67b87a0cfa4
zakkysurya <zakkysurya@gmail.com>
2022-09-21 05:51:19 +0700

    Datepicker

--- public/source/assets/js/pages/datepickers.js
+++ public/source/assets/js/pages/datepickers.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
     "use strict";
     $(".flatpickr1").flatpickr();
 
@@ -7,5 +6,15 @@ $(document).ready(function() {
         enableTime: true,
         dateFormat: "Y-m-d H:i",
     });
-    
+
+    $(".flatpickr3").flatpickr({
+        plugins: [
+            new monthSelectPlugin({
+                //shorthand: true, //defaults to false
+                dateFormat: "Y", //defaults to "F Y"
+                altFormat: "Y", //defaults to "F Y"
+                theme: "dark", // defaults to "light"
+            }),
+        ],
+    });
 });
