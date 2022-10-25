3bf96f687841dba55e95e1ba0c0f54d6ac097468
zakkysurya <zakkysurya@gmail.com>
2022-10-26 05:59:06 +0700

    Frontend Controller Updated

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -8,46 +8,54 @@ use App\Models\Portfolio;
 use App\Models\Profile;
 use App\Models\Service;
 use App\Models\Skill;
+use App\Models\Testimonial;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function index(){
-        $dataProfile = Profile::first();
-        $dataSkills  = Skill::all();
-        $dataExperiences    = Experience::all();
-        $dataEducation      = Education::all();
-        $dataServices       = Service::all();
-        $dataPortfolio      = Portfolio::all();
-
-        $data = [
-            "home" => [
-                'full_name' => $dataProfile->full_name,
-                'nick_name' => $dataProfile->nick_name,
-                'introduction' => $dataProfile->job_description,
-                'email' => $dataProfile->email,
-                'fb' => $dataProfile->fb,
-                'ig' => $dataProfile->ig,
-                'tweeter' => $dataProfile->tweeter,
-                'linkid' => $dataProfile->linkid,
-            ],
-            "about_me" => [
-                'photo'         => $dataProfile->photo,
-                'self_description' => $dataProfile->self_description,
-                'skills'        => $dataSkills,
-                'experiences'   => $dataExperiences,
-                'educations'    => $dataEducation,
-            ],
-            "services"      => [ "services" => $dataServices],
-            "work"          => [ 'images' => $dataPortfolio ],
-            "testimonial"   => [ 'testimonials' => $dataPortfolio ],
-            "contact" => [
-                'address'   => $dataProfile->address,
-                'phone'     => $dataProfile->phone,
-                'email'     => $dataProfile->email,
-            ],
-        ];
-        // dd($data);
+        try {
+            $dataProfile = Profile::first();
+            $dataSkills  = Skill::all();
+            $dataExperiences    = Experience::all();
+            $dataEducation      = Education::all();
+            $dataServices       = Service::all();
+            $dataPortfolio      = Portfolio::all();
+            $dataTestimonial    = Testimonial::all();
+    
+            $data = [
+                "home" => [
+                    'photo'     => $dataProfile->photo,
+                    'full_name' => $dataProfile->full_name,
+                    'nick_name' => $dataProfile->nick_name,
+                    'position' => $dataProfile->position,
+                    'job_description' => $dataProfile->job_description,
+                    'email' => $dataProfile->email,
+                    'fb' => $dataProfile->fb,
+                    'ig' => $dataProfile->ig,
+                    'tweeter' => $dataProfile->tweeter,
+                    'linkid' => $dataProfile->linkid,
+                ],
+                "about_me" => [
+                    'photo'         => $dataProfile->photo,
+                    'self_description' => $dataProfile->self_description,
+                    'skills'        => $dataSkills,
+                    'experiences'   => $dataExperiences,
+                    'educations'    => $dataEducation,
+                ],
+                "services"      => $dataServices,
+                "work"          => $dataPortfolio,
+                "testimonial"   => $dataTestimonial,
+                "contact" => [
+                    'address'   => $dataProfile->address,
+                    'phone'     => $dataProfile->phone,
+                    'email'     => $dataProfile->email,
+                ],
+            ];
+        } catch (QueryException $e) {
+            $data = [ "errors" => true, "msg" => $e->errorInfo ];
+        }
         return view('web.index', $data);
     }
 }
